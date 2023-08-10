2938ef8acbb718489a5e5ba5061b82696c2fda2c
Talha-55 <talha.rauf@purelogics.com>
2023-08-10 13:57:47 +0500

    routes updates

---
+++ routes/admin/player.routes.js
@@ -0,0 +1,15 @@
+
+const express = require('express');
+const { getAllPlayers, searchPlayres, createPlayerProfile, getPlayerById, editPlayer, deletePlayer } = require('../../controllers/admin/player.controller');
+const router = express.Router();
+
+
+router.post("/createPlayerProfile", createPlayerProfile);
+router.get("/getPlayerById/:id", getPlayerById);
+router.patch("/editPlayer/:id", editPlayer);
+router.delete("/deletePlayer/:id", deletePlayer)
+router.get("/getAllPlayers", getAllPlayers);
+router.get("/searchPlayer", searchPlayres);
+
+
+module.exports = router;
